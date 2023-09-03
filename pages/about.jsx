244aca55ac73d465d1cb7c23db8fa442af7604eb
Dilip Kumar <curiousdilip@gmail.com>
2023-09-03 22:06:01 +0530

    clean up

---
+++ pages/about.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>About | Ramen Paul</title>
+                <meta name="description" content="Hi, I’m Ramen Paul, Authorized teacher of Trinity College, London." />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <main>
+                <h1>about</h1>
+            </main>
+        </>
+    )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ramen Paul – We are Nothing</title>
+        <meta name="description" content="Hi, I’m Ramen Paul, Authorized teacher of Trinity College, London." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <h1>home</h1>
+      </main>
+    </>
+  )
+}
