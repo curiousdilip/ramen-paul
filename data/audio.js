669574ea8f3991cdf6cb285eca06dfd76efee61d
Dilip Kumar <curiousdilip@gmail.com>
2023-09-09 10:48:45 +0530

    audio added

--- data/audio.js
+++ data/audio.js
@@ -11,5 +11,40 @@ export const audio = [
     }, {
         src: "/audio/D4-singing-exercise-1.mp3",
         title: "D4 Singing Excercise 1"
-    },
+    }
+    , {
+        src: "/audio/easy-playback-21.mp3",
+        title: "Easy Playback"
+    }, {
+        src: "/audio/harmony-1.5.mp3",
+        title: "Harmony 1.5"
+    }, {
+        src: "/audio/harmony-1.mp3",
+        title: "Harmony 1"
+    }, {
+        src: "/audio/harmony-2.mp3",
+        title: "Harmony 2"
+    }, {
+        src: "/audio/harmony.mp3",
+        title: "Harmony"
+    }, {
+        src: "/audio/play-back-3-by-ramen-sir.mp3",
+        title: "Play Back 3 "
+    }, {
+        src: "/audio/play-back-11.mp3",
+        title: "Play Back 11"
+    }, {
+        src: "/audio/play-back-zoro-1.mp3",
+        title: "Play Back Zoro 1"
+    }, {
+        src: "/audio/playback-ramen-1.mp3",
+        title: "Playback Ramen"
+    }, {
+        src: "/audio/practice-playback.mp3",
+        title: "Practice Playback"
+    }, {
+        src: "/audio/Playback-Sep15-guitar.mp3",
+        title: "Playback Guitar Sept 15 "
+    }
+
 ]
