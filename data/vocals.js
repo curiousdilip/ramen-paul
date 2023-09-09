be791ff6306f97156f54fac3d58f3ab41ebd89d5
Dilip Kumar <curiousdilip@gmail.com>
2023-09-09 12:20:50 +0530

    Update vocals.js

--- data/vocals.js
+++ data/vocals.js
@@ -4,4 +4,85 @@ export const vocalsVideos = [
     }, {
         id: "0IxUAwTKBpo",
     }
+
+
+
+    , {
+        id: "bP-zUGQpyDQ",
+    }
+    , {
+        id: "951yZ7LXGd8",
+    }, {
+        id: "Mvvw0qmouh0",
+    }, {
+        id: "22nHyscXgrE",
+    }, {
+        id: "6OHHXUd8clg",
+    }, {
+        id: "ruyIumF5HnU",
+    }, {
+        id: "ZYUaF1eQ_cs",
+    }, {
+        id: "8GIkvAONzH4",
+    }, {
+        id: "oQ0Eb4ky-Zc",
+    }, {
+        id: "Pm8njvbYjH4",
+    }, {
+        id: "zVT9oYRTTbM",
+    }, {
+        id: "gkrbBwZ5ul8",
+    }, {
+        id: "qI6SSABwG_U",
+    }, {
+        id: "C0YnHGzxRVY",
+    }, {
+        id: "4sgKL2zJu8s",
+    }, {
+        id: "KsutG8bGuAY",
+    }, {
+        id: "FsSAyu2A2Ek",
+    }, {
+        id: "iN9LA64b9-I",
+    }, {
+        id: "6nqXzP03oy4",
+    }, {
+        id: "OPcK9bHOoRE",
+    }, {
+        id: "oOv2X8MfvZA",
+    }, {
+        id: "LjKWnhoA4ks",
+    }
+
+    // , {
+    //     id: "JTsg23lAViY",
+    // }
+    , {
+        id: "yf_fG59LFWk",
+    }
+
+    , {
+        id: "ni5HYZetdeM",
+    }, {
+        id: "EwUUohRq6Ks",
+    }, {
+        id: "IFAJOOz4JiQ",
+    }, {
+        id: "VhEuC1tBLo0",
+    }, {
+        id: "rkKS8Bp70rw",
+    }, {
+        id: "CM8LEJchG0U",
+    }, {
+        id: "RAfPUK43UmY",
+    }, {
+        id: "bE7_1c5Z0Hs",
+    }, {
+        id: "u0kk2TXjutw",
+    }, {
+        id: "6g4ZXIJ17uw",
+    }
+    // , {
+    //     id: "KMvuCBO1xCI",
+    // }
 ]
