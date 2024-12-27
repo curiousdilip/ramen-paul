94a426cbf3d050510aa5d4a64a5dae5fcc9dd2fa
Dilip Kumar Maurya <curiousdilip@gmail.com>
2024-12-27 19:30:05 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['img.youtube.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        pathname: '**',
+      },
+    ],
   },
   reactStrictMode: true,
   async headers() {
